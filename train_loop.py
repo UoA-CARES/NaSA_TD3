12800772ee3d8ced058b6274cb2a4b37eed2802d
david <davidvalenciaredrovan@gmail.com>
2023-07-13 15:36:08 +1200

    remove plot each evaluation

--- train_loop.py
+++ train_loop.py
@@ -185,8 +185,8 @@ def evaluation_loop(env, agent, frames_stack, total_counter, file_name):
         episode_reward += reward_extrinsic
         video.write(grab_frame(env))
         if done:
-            original_img, reconstruction = agent.get_reconstruction_for_evaluation(state)
-            plot_reconstruction_img(original_img, reconstruction)
+            # original_img, reconstruction = agent.get_reconstruction_for_evaluation(state)
+            # plot_reconstruction_img(original_img, reconstruction)
             logging.info(f" EVALUATION | Eval Episode was completed with {episode_timesteps} steps | Reward= {episode_reward:.3f}")
             state = frames_stack.reset()
             episode_reward = 0
